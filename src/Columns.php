46d2d4055d9079dfc4988e4b55a3fa99d73b0929
André Heeke <andre@nexper.de>
2017-06-18 01:53:12 +0200

    Initial commit

---
+++ src/Columns.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Copyright NEXPER <info@nexper.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace MadeYourDay\RockSolidColumns;
+
+/**
+ * Nexper Columns DCA (tl_content and tl_module)
+ *
+ * Provide miscellaneous methods that are used by the data configuration arrays.
+ *
+ * @author André Heeke <andre@nexper.de>
+ */
+class Columns
+{
+}
---
+++ src/NexperColumnsBundle.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Copyright NEXPER <info@nexper.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nexper\NexperColumns;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Configures the Nexper Columns bundle.
+ *
+ * @author André Heeke <andre@nexper.de>
+ */
+class RockSolidColumnsBundle extends Bundle
+{
+}
