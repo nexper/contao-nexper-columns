748a45fa66009186fc5e87d7e21b3c000987e85c
André Heeke <andre.heeke@icloud.com>
2017-06-21 22:36:10 +0200

    Change reset css option to subpalette

--- src/Resources/contao/dca/tl_layout.php
+++ src/Resources/contao/dca/tl_layout.php
@@ -6,17 +6,20 @@
  * file that was distributed with this source code.
  */
 
-$GLOBALS['TL_DCA']['tl_layout']['palettes']['default'] .= ';{nx_columns_legend},nx_columns_load_reset_css,nx_columns_load_css';
+$GLOBALS['TL_DCA']['tl_layout']['palettes']['__selector__'][] = 'nx_columns_load_css';
+$GLOBALS['TL_DCA']['tl_layout']['palettes']['default'] .= ';{nx_columns_legend},nx_columns_load_css';
+$GLOBALS['TL_DCA']['tl_layout']['subpalettes']['nx_columns_load_css'] = 'nx_columns_load_reset_css';
+
+$GLOBALS['TL_DCA']['tl_layout']['fields']['nx_columns_load_css'] = array(
+	'inputType' => 'checkbox',
+	'label' => &$GLOBALS['TL_LANG']['tl_layout']['nx_columns_load_css'],
+    'eval' => array('submitOnChange' => true, 'tl_class' =>'w50'),
+	'sql' => "char(1) NOT NULL default '1'",
+);
 
 $GLOBALS['TL_DCA']['tl_layout']['fields']['nx_columns_load_reset_css'] = array(
 	'inputType' => 'checkbox',
 	'label' => &$GLOBALS['TL_LANG']['tl_layout']['nx_columns_load_reset_css'],
     'eval' => array('tl_class' => 'w50'),
 	'sql' => "char(1) NOT NULL default '0'",
-);
-$GLOBALS['TL_DCA']['tl_layout']['fields']['nx_columns_load_css'] = array(
-	'inputType' => 'checkbox',
-	'label' => &$GLOBALS['TL_LANG']['tl_layout']['nx_columns_load_css'],
-    'eval' => array('tl_class' =>'w50'),
-	'sql' => "char(1) NOT NULL default '1'",
 );
