b2b54b7ff2e28630e86945a07b59876d78edeb13
André Heeke <andre.heeke@icloud.com>
2017-08-03 00:15:44 +0200

    Move contao manager plugin to another directory

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Copyright NEXPER <info@nexper.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nexper\ColumnsBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Nexper\ColumnsBundle\NexperColumnsBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
+use Symfony\Component\Config\Loader\LoaderResolverInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+/**
+ * Plugin for the Contao Manager.
+ *
+ * @author Nexper <https://github.com/nexper>
+ */
+class Plugin implements BundlePluginInterface, RoutingPluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(NexperColumnsBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
+}
